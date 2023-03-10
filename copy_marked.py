3c46dce0bd2b2e3237f9168a63f3766088fcf2ff
Lee Hall <lhall@javafoundry.com>
2023-03-10 10:06:11 -0500

    Copy util is cross platform

--- copy_marked.py
+++ copy_marked.py
@@ -1,7 +1,22 @@
 import sqlite3
 from shutil import copy
+import os
+import sys
+import re
 
-with sqlite3.connect("pfnss.db") as db:
+if len(sys.argv) not in [3, 5]:
+    print(f"Usage: {sys.argv[0]} <db> <dest dir> [<replace pattern> <replacement>]", file=sys.stderr)
+    raise SystemExit()
+
+db_name = sys.argv[1]
+destdir = sys.argv[2]
+
+repl_pattern = None
+if len(sys.argv) == 5:
+    repl_pattern = re.compile(sys.argv[3])
+    replacement = sys.argv[4]
+
+with sqlite3.connect(db_name) as db:
     marked = db.execute('''select name, mark from files, marks
     where mark <> '' and files.id = file_id order by mark''').fetchall()
 
@@ -12,5 +27,9 @@ for fn, mark in marked:
         subdir = "Delete"
     else:
         subdir = "Edit"
+    if repl_pattern:
+        fn = re.sub(repl_pattern, replacement, fn)
     print(f"{fn} is marked {mark}")
-    copy(fn, f"c:/Users/phall/OneDrive/Desktop/Marked Pictures/{subdir}")
+    dir = f"{destdir}/{subdir}"
+    os.makedirs(dir, exist_ok=True)
+    copy(fn, dir)
