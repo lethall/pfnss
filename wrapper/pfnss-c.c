c0283dee9e2ff62e5424b13fdeeb106738b48714
Lee Hall <lhall@javafoundry.com>
2023-11-21 16:05:03 -0500

    Simple C wrapper

---
+++ wrapper/pfnss-c.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <cstring>
+
+#define BUFSIZE 128
+
+int main(int argc, char **argv) {
+    FILE *f = fopen("pfnss-c.txt", "r");
+    char *buf = (char *)calloc(BUFSIZE, sizeof(char));
+    fread(buf, sizeof(char), BUFSIZE - 1, f);
+    fclose(f);
+    char *s = buf;
+    while (*s) {
+        if (*s == '\n') {
+            *s++ = ' ';
+            *s = '\0';
+            break;
+        }
+        ++s;
+    }
+    --argc;
+    ++argv;
+    while (argc--) {
+        strcat(buf, *argv++);
+        if (argc) {
+            strcat(buf, " ");
+        }
+    }
+    printf("Executing: '%s'\n", buf);
+    int retcode = system(buf);
+    free(buf);
+    return retcode;
+}
